b674132982a513370ce89fade13020517645ece8
Pest270904 <ntpllc2004@gmail.com>
2024-05-21 01:29:10 +0700

    update lab4

--- Lab4/pwd_gen.py
+++ Lab4/pwd_gen.py
@@ -1,21 +1,19 @@
 username = input("---> Enter your username: ")
 # username = "399076724"
 
-v8 = "cRVlg" # random string
+randomStr = "cRVlg" # random string
 newStr = ''
 
 for i in range(0, len(username)): 
     if i > 1:
         if i > 3:
-            newStr += v8[8-i]
+            newStr += randomStr[8-i]
         else:
             newStr += username[i+2]
     else:
         newStr += username[i+5]
 
 print("(+) New generated string: " + newStr)
-
-from math import ceil
 
 pwd = ''
 
@@ -24,7 +22,7 @@ while True:
     v3 = len(username)
     if v3 <= i:
         break
-    v22 = ceil(float((ord(username[i]) + ord(newStr[i]))) // 2) 
+    v22 = (ord(username[i]) + ord(newStr[i])) // 2
     pwd += chr(v22)
     i += 1
 
